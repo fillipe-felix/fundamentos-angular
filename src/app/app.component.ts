4131ee7e5cbcdb593c8ddb3ea0a21326ec3ab5ae
fillipe.felix <felipesoares_1993@hotmail.com>
2024-12-16 20:27:42 -0300

    97 - Simplifique o modelo de componente de aplicativo substituindo o conteúdo do placeholder.
    Removidos dados extensos de HTML, CSS e template do placeholder em `app.component.html`. Introduzidos elementos básicos de entrada para demonstrar a funcionalidade de detecção de alterações do Angular com vinculações de exemplo e um manipulador de botões.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fundamentos-angular';
+  teste: string = 'Fillipe';
+
+  constructor(private _cdRef: ChangeDetectorRef) {
+  }
+
+  changeDetection() {
+    this._cdRef.detectChanges();
+  }
 }
